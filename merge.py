b95e8b6f2b46d1746798b6639f7daaa203afe3f4
meishima <meishimma@gmail.com>
2025-06-24 21:02:43 +0300

    Tile adding system created

---
+++ merge.py
@@ -0,0 +1,27 @@
+import random
+
+rows, cols = (5, 5)
+array = [[0 for _ in range(cols)] for _ in range(rows)] 
+
+def showArray(arrayName):
+    for row in arrayName:
+        print(row)
+    print("\n")
+    
+def addTile():
+    filledTiles = []
+    for row in range(len(array)):
+        for col in range(len(array[row])):
+            if(array[row][col] == 0):
+                filledTiles.append([row,col])
+    if(len(filledTiles) > 0):
+        randomTileIndex = random.randint(0,len(filledTiles)-1) # added -1 because of len() is not 0-indexed
+        randomTileIndex_x = filledTiles[randomTileIndex][0]
+        randomTileIndex_y = filledTiles[randomTileIndex][1]
+        array[randomTileIndex_x][randomTileIndex_y] = 1
+    else:
+        print("Error. Can't add more more tiles to the board.")
+    showArray(array)
+
+addTile()
+
